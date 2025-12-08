bce1c02bd14e5cbf014c13577f2134e486085ff8
mugeankay-dotcom <mugeankay@gmail.com>
2025-12-08 21:39:38 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXfHWpe7Pd1yj8lCt8mRw7RBv0x1e6r4E",
+  authDomain: "finanstakip-f2dca.firebaseapp.com",
+  projectId: "finanstakip-f2dca",
+  storageBucket: "finanstakip-f2dca.firebasestorage.app",
+  messagingSenderId: "431970136870",
+  appId: "1:431970136870:web:e8eff811609197ff2d87e7",
+  measurementId: "G-ZWXEXXW5YT"
+};
+
+// Bu dosya yüklendiğinde konsola bilgi verelim
+console.log('Firebase Config loaded. ☁️');
+
+// Bu dosya yüklendiğinde konsola bilgi verelim
+console.log('Firebase Config loaded.');
---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'finans-takip-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
